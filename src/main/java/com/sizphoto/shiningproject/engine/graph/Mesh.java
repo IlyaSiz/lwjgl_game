23d4e165dd91920c032b69052bba0c77335e540a
Ilya Siz <ilyasiz@gmail.com>
2019-12-27 01:22:36 +0300

    Attribute locations set-up upon Mesh creation

--- src/main/java/com/sizphoto/shiningproject/engine/graph/Mesh.java
+++ src/main/java/com/sizphoto/shiningproject/engine/graph/Mesh.java
@@ -10,7 +10,27 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
 
-import static org.lwjgl.opengl.GL30.*;
+import static org.lwjgl.opengl.GL11.GL_FLOAT;
+import static org.lwjgl.opengl.GL11.GL_TEXTURE_2D;
+import static org.lwjgl.opengl.GL11.GL_TRIANGLES;
+import static org.lwjgl.opengl.GL11.GL_UNSIGNED_INT;
+import static org.lwjgl.opengl.GL11.glBindTexture;
+import static org.lwjgl.opengl.GL11.glDrawElements;
+import static org.lwjgl.opengl.GL13.GL_TEXTURE0;
+import static org.lwjgl.opengl.GL13.glActiveTexture;
+import static org.lwjgl.opengl.GL15.GL_ARRAY_BUFFER;
+import static org.lwjgl.opengl.GL15.GL_ELEMENT_ARRAY_BUFFER;
+import static org.lwjgl.opengl.GL15.GL_STATIC_DRAW;
+import static org.lwjgl.opengl.GL15.glBindBuffer;
+import static org.lwjgl.opengl.GL15.glBufferData;
+import static org.lwjgl.opengl.GL15.glDeleteBuffers;
+import static org.lwjgl.opengl.GL15.glGenBuffers;
+import static org.lwjgl.opengl.GL20.glDisableVertexAttribArray;
+import static org.lwjgl.opengl.GL20.glEnableVertexAttribArray;
+import static org.lwjgl.opengl.GL20.glVertexAttribPointer;
+import static org.lwjgl.opengl.GL30.glBindVertexArray;
+import static org.lwjgl.opengl.GL30.glDeleteVertexArrays;
+import static org.lwjgl.opengl.GL30.glGenVertexArrays;
 
 public class Mesh {
 
@@ -56,6 +76,7 @@ public class Mesh {
       posBuffer.put(positions).flip();
       glBindBuffer(GL_ARRAY_BUFFER, vboId);
       glBufferData(GL_ARRAY_BUFFER, posBuffer, GL_STATIC_DRAW);
+      glEnableVertexAttribArray(0);
       // Define structure of the data
       glVertexAttribPointer(0, 3, GL_FLOAT, false, 0, 0);
 
@@ -66,6 +87,7 @@ public class Mesh {
       textCoordsBuffer.put(textCoords).flip();
       glBindBuffer(GL_ARRAY_BUFFER, vboId);
       glBufferData(GL_ARRAY_BUFFER, textCoordsBuffer, GL_STATIC_DRAW);
+      glEnableVertexAttribArray(1);
       glVertexAttribPointer(1, 2, GL_FLOAT, false, 0, 0);
 
       // Vertex normals VBO
@@ -80,6 +102,7 @@ public class Mesh {
       }
       glBindBuffer(GL_ARRAY_BUFFER, vboId);
       glBufferData(GL_ARRAY_BUFFER, vecNormalsBuffer, GL_STATIC_DRAW);
+      glEnableVertexAttribArray(2);
       glVertexAttribPointer(2, 3, GL_FLOAT, false, 0, 0);
 
       // Index VBO
@@ -130,16 +153,10 @@ public class Mesh {
 
     // Draw the mesh
     glBindVertexArray(getVaoId());
-    glEnableVertexAttribArray(0);
-    glEnableVertexAttribArray(1);
-    glEnableVertexAttribArray(2);
   }
 
   private void endRender() {
     // Restore state
-    glDisableVertexAttribArray(0);
-    glDisableVertexAttribArray(1);
-    glDisableVertexAttribArray(2);
     glBindVertexArray(0);
 
     glBindTexture(GL_TEXTURE_2D, 0);
